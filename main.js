b8d733543febe1b0ee2f9362b3a737110d3d3210
paulafiaschi <paul6399@stud.kea.dk>
2022-04-05 10:13:41 +0200

    copy to clipboard npm

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./sass/style.scss";
+
+new ClipboardJS(".email-btn");
+
+document.querySelector(".copy").addEventListener("click", changeIcon);
+
+function changeIcon() {
+  console.log("email copied");
+  document.querySelector(".copy").innerHTML = "check";
+  document.querySelector(".copy").style.backgroundColor = "rgb(71, 175, 71)";
+  document.querySelector(".copy").style.color = "white";
+  setTimeout(() => {
+    document.querySelector(".copy").innerHTML = "content_copy";
+    document.querySelector(".copy").style.color = "var(--color-dark)";
+    document.querySelector(".copy").style.backgroundColor = "var(--color-light)";
+  }, 700);
+}
